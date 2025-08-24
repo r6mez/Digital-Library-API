8b9270beeff1012145abf1e08ada6f7bea5a4975
Mostafa Mahmoud <157222523+Mostafa2115@users.noreply.github.com>
2025-08-24 21:24:13 +0300

    add transaction controller

---
+++ controllers/transactionController.js
@@ -0,0 +1,83 @@
+const Transaction = require('../models/TransactionModel');
+const asyncHandler = require('../utils/asyncHandler');
+
+
+// Create a new transaction
+const createTransaction = asyncHandler(async (req, res) => {
+    const { bookId, amount, type, description } = req.body;
+
+    const transaction = await Transaction.create({
+        user: req.body.id,
+        book: bookId,
+        type,
+        amount,
+        description,
+        createdAt: Date.now()
+    });
+
+    res.status(201).json({
+        success: true,
+        data: transaction
+    });
+});
+
+// Get a transaction by ID
+const getTransactionById = asyncHandler(async (req, res) => {
+    const transaction = await Transaction.findById(req.params.id).populate('user book');
+
+    if (!transaction) {
+        return res.status(404).json({
+            success: false,
+            message: 'Transaction not found'
+        });
+    }
+
+    res.status(200).json({
+        success: true,
+        data: transaction
+    });
+});
+
+// Update a transaction
+const updateTransaction = asyncHandler(async (req, res) => {
+    const transaction = await Transaction.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    if (!transaction) {
+        return res.status(404).json({
+            success: false,
+            message: 'Transaction not found'
+        });
+    }
+
+    res.status(200).json({
+        success: true,
+        data: transaction
+    });
+});
+
+// Delete a transaction
+const deleteTransaction = asyncHandler(async (req, res) => {
+    const transaction = await Transaction.findByIdAndDelete(req.params.id);
+
+    if (!transaction) {
+        return res.status(404).json({
+            success: false,
+            message: 'Transaction not found'
+        });
+    }
+
+    res.status(204).json({
+        success: true,
+        data: null
+    });
+});
+
+module.exports = {
+    createTransaction,
+    getTransactionById,
+    updateTransaction,
+    deleteTransaction
+};
