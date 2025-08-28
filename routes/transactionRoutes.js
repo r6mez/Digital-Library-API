d5ab9b59610f3b384037566cb238e0909073da2c
Mostafa Mahmoud <157222523+Mostafa2115@users.noreply.github.com>
2025-08-28 23:16:36 +0300

    fix small part

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -7,9 +7,10 @@ const {transactionSchema} = require('../validators/transactionValidator');
 const validate = require('../validators/validate');
 const { admin } = require('../middleware/adminMiddleware');
 
-router.get('/', protect, getTransactionById);
+router.get('/:id', protect, getTransactionById);
 router.post('/', protect, validate(transactionSchema), createTransaction);
 router.put('/:id', protect, validate(transactionSchema), updateTransaction);
 router.delete('/:id', protect, validate(transactionSchema), deleteTransaction);
 
 module.exports = router;
+
