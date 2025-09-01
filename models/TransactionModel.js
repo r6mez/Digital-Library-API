8382c155304bf42f4feeab885cf38cf23d45887a
Ramez Medhat <iramezdev@gmail.com>
2025-09-01 17:27:46 +0300

    fix 2

--- models/TransactionModel.js
+++ models/TransactionModel.js
@@ -1,23 +1,22 @@
+const mongoose = require('mongoose');
 
-    const mongoose = require('mongoose');
+const transactionSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    amount: { 
+        type: Number,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String
+    }
+}, { timestamps: { createdAt: true, updatedAt: false } });
 
-    const transactionSchema = new mongoose.Schema({
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-            required: true
-        },
-        amount: { 
-            type: Number,
-            required: true
-        },
-        type: {
-            type: String,
-            required: true,
-        },
-        description: {
-            type: String
-        }
-    }, { timestamps: { createdAt: true, updatedAt: false } });
-
-    module.exports = mongoose.models.Transaction || mongoose.model('Transaction', transactionSchema);
+module.exports = mongoose.model('Transaction', transactionSchema);
